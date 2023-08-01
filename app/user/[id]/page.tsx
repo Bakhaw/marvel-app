77bbc60af38942c764e7e739c95ea1a56f344b9d
Faïssal <fhattou.ext@unowhy.com>
2023-08-01 14:42:11 +0200

    Add user page

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,5 +1,36 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+import { formatPostCard, formatProfileCard } from "@/app/lib";
+import { users } from "@/app/mock/users";
+
+import PostCard from "@/app/components/PostCard";
+import ProfileCard from "@/app/components/ProfileCard";
+
 function Page() {
-  return <div>User page.</div>;
+  const { id: userId } = useParams();
+  const user = users.find((user) => user.id === userId);
+
+  if (!user) return null;
+
+  return (
+    <div className="min-h-screen flex flex-col items-center py-12">
+      <div className="mb-16">
+        <ProfileCard {...formatProfileCard(user)} />
+      </div>
+
+      <ul className="space-y-6">
+        {Array(32)
+          .fill("")
+          .map((d, i) => (
+            <li key={i}>
+              <PostCard {...formatPostCard(user)} />
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Page;
