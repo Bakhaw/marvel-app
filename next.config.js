2144bf924bfb6b41f1294c8cd6fb02ad5ef698fb
Hattou Faïssal <faissal.hattou@gmail.com>
2023-08-15 03:33:31 +0200

    working on pwa config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-const withPWA = require("next-pwa")({
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const nextConfig = withPWA({
   dest: "public",
   register: true,
+  runtimeCaching,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+  // disable: process.env.NODE_ENV === "development",
   images: {
     domains: ["i.annihil.us"], // images coming from marvel api
   },
-};
+});
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
