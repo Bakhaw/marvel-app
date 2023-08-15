e26e037db543ad0535bbeff25fd0b02e920b69d2
Hattou Faïssal <faissal.hattou@gmail.com>
2023-08-15 02:31:55 +0200

    adding pwa support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.annihil.us"],
+    domains: ["i.annihil.us"], // images coming from marvel api
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
